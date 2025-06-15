ee7c0bc48ba367b81d58a96abbd7069b59d2bae6
Tahseena Badat <tahseena@usf.edu>
2025-06-15 18:39:58 +0200

    Product objects created

---
+++ cc_4.js
@@ -0,0 +1,31 @@
+const products = [
+{
+    name: "Laptop", 
+    category:"Electronics",
+    price: 1200,
+    inventory: 10
+}, 
+{ 
+    name: "Heather Grey Sweater",
+    category: "Apparel",
+    price: 30,
+    inventory: 50
+}, 
+{
+    name: "Banana Bunch",
+    category: "Groceries",
+    price: 3,
+    inventory: 100
+}, 
+{ 
+    name: "XL Large Rug",
+    category: "Household",
+    price: 750,
+    inventory: 5
+},
+{ 
+    name: "Star Screwdriver",
+    category: "Hardware",
+    price: 7,
+    inventory: 25
+}];
